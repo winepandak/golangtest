1fcf6f8607521d85994a4b37bf3b66425fabdeb4
kervant <187225207@qq.com>
2019-10-15 17:37:16 +0800

    firstcommit

---
+++ src/test/Utils.go
@@ -0,0 +1,5 @@
+package main
+
+func testUtils() (int, int) {
+	return 100, 200
+}
---
+++ src/test/mian.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	//i,j:=0,"string?";
+	//fmt.Println("hello world","123",i,j)
+	//s,_ := getTextNUm();
+	//fmt.Println(s)
+	s, _ := testUtils()
+	fmt.Println(s)
+
+	testHttpHost()
+}
+func getTextNUm() (int, int) {
+	return 100, 200
+}
