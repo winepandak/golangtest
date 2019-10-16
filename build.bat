1a327dba53852cc2d41d2fc17cb6f49c0aba6d35
kervant <187225207@qq.com>
2019-10-16 13:43:12 +0800

    bat fix3

--- build.bat
+++ build.bat
@@ -8,20 +8,22 @@ echo "ssh_host=$1"
 #ssh_host="root@149.129.175.159"    #india
 
 #ssh_host="root@47.56.130.152"    #hk test
-ssh -p22 root@47.56.130.152
+#ssh -p22 root@47.56.130.152
 
-ssh $ssh_host 'rm -rf /opt/temp'
-ssh $ssh_host 'mkdir /opt/testproject'
-ssh $ssh_host 'mkdir /opt/temp'
+set %ssh_host = "-p22 root@47.56.130.152"
+
+ssh %ssh_host 'rm -rf /opt/temp'
+ssh %ssh_host 'mkdir /opt/testproject'
+ssh %ssh_host 'mkdir /opt/temp'
 
 
 
-ssh $ssh_host 'git clone https://187225207@qq.com:Edifieal1716@github.com/winepandak/golangtest.git /opt/testproject'
+ssh %ssh_host 'git clone https://187225207@qq.com:Edifieal1716@github.com/winepandak/golangtest.git /opt/testproject'
 #ssh $ssh_host 'git clone https://187225207@qq.com:Edifieal1716@github.com/winepandak/golangtest.git /opt/temp/common'
 
-ssh $ssh_host '\cp -rf /opt/temp/nua/start.sh /opt/testproject/'
-ssh $ssh_host '\cp -rf /opt/temp/nua/stop.sh /opt/testproject/'
-ssh $ssh_host '\cp -rf /opt/temp/nua/grace_restart.sh /opt/testproject/'
+ssh %ssh_host '\cp -rf /opt/temp/nua/start.sh /opt/testproject/'
+ssh %ssh_host '\cp -rf /opt/temp/nua/stop.sh /opt/testproject/'
+ssh %ssh_host '\cp -rf /opt/temp/nua/grace_restart.sh /opt/testproject/'
 
 #if ssh $ssh_host test -e /opt/testproject/nua.json;
 #then
@@ -30,8 +32,8 @@ ssh $ssh_host '\cp -rf /opt/temp/nua/grace_restart.sh /opt/testproject/'
 #    ssh $ssh_host '\cp -rf /opt/testproject/nua.json /opt/testproject/'
 #fi
 
-ssh $ssh_host 'chmod +x /opt/testproject/*.sh'
+ssh %ssh_host 'chmod +x /opt/testproject/*.sh'
 
-ssh $ssh_host '/opt/testproject/nua_git.sh'
+ssh %ssh_host '/opt/testproject/nua_git.sh'
 
-ssh $ssh_host '/opt/testproject/grace_restart.sh'
+ssh %ssh_host '/opt/testproject/grace_restart.sh'
